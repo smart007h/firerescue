bde67bd9821c9b608323182a37079c52f1996aa7
SARFO99 <Sarfosilas73@gmail.com>
2025-09-02 02:05:26 +0000

    Clean up: Remove unrelated analysis, debug, fix, and temporary files

--- webpack.config.js
+++
@@ -1,6 +0,0 @@
-import createExpoWebpackConfigAsync from '@expo/webpack-config';
-
-export default async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
-  return config;
-}; 
